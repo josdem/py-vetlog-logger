3f2f2da632e3e6c5b603451c8a1858978649d06d
josdem <joseluis.delacruz@gmail.com>
2024-04-29 07:13:31 -0400

    Adding create test log

--- python/create_log.py
+++ python/create_log.py
@@ -1,6 +1,23 @@
+import os
+import logging
 
 class Logger:
     def __init__(self, path):
         self.path = path
-        file = open(path, "a")
-        file.close()
+        self.prepare_file()
+        self.logger = logging.getLogger(path)
+        self.logger.setLevel(logging.INFO)
+        self.create_handler()
+    
+    def prepare_file(self):
+        if not os.path.exists(self.path):
+            open(self.path, 'a')
+
+    def create_handler(self):
+        self.formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+        file_handler = logging.FileHandler(self.path)
+        self.logger.addHandler(file_handler)
+        file_handler.setFormatter(self.formatter)
+
+    def get_logger(self):
+        return self.logger        
--- tests/test_create_log.py
+++ tests/test_create_log.py
@@ -5,5 +5,11 @@ from python.create_log import *
 class FixedTest(unittest.TestCase):
     path = "vetlog.log"
     def test_create_logger(self):
-        Logger(self.path)
-        self.assertTrue(os.path.isfile(self.path))
+        log = Logger(self.path)
+        test_logger = log.get_logger()
+        test_logger.info("Test log")
+        assert os.path.exists(self.path)
+        file = open(self.path, 'r')
+        assert "Test log" in file.read()
+        file.close()
+        
