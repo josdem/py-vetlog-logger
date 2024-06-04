c23df5cbdd27747b0a217265fc740a1da4a6e353
josdem <joseluis.delacruz@gmail.com>
2024-06-04 16:51:24 -0400

    #12 Adding test log

---
+++ tests/test_mock_log.py
@@ -0,0 +1,10 @@
+from unittest.mock import MagicMock
+from python.create_log import *
+import unittest
+
+class FixedTest(unittest.TestCase):
+    path = "vetlog.log"
+    def test_create_logger(self):
+        log = Logger(self.path)
+        log.get_logger = MagicMock()
+       
