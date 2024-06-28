64d60d54739672f9d3c8dbf2acbb40cb9a2e1878
josdem <joseluis.delacruz@gmail.com>
2024-06-28 17:12:24 -0400

    #2 Pimping some methods

--- tests/test_mox_log.py
+++ tests/test_mox_log.py
@@ -4,12 +4,12 @@ import os
 class TestOs(mox.MoxTestBase):
     def test_getcwd(self):
         self.mox.StubOutWithMock(os, 'getcwd')
-        # calls
+
         os.getcwd().AndReturn('/mox/path')
 
-        self.mox.ReplayAll()
-        self.assertEqual(os.getcwd(), '/mox/path')
-        self.mox.VerifyAll()
+        self.mox.replay_all()
+        assert os.getcwd() == '/mox/path'
+        self.mox.verify_all()
 
 
 if __name__ == '__main__':
