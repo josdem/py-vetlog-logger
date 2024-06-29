380317baf759421b62f4fd4a07168e2c10e9ac8c
josdem <joseluis.delacruz@gmail.com>
2024-06-29 10:43:43 -0400

    #2 grouping calls

--- tests/test_mox_log.py
+++ tests/test_mox_log.py
@@ -6,12 +6,15 @@ class FixedTest(mox.MoxTestBase):
     path = "vetlog.log"
     def test_create_logger(self):
         logger = self.mox.CreateMockAnything()
+        
         self.mox.StubOutWithMock(logging, 'getLogger')
         self.mox.StubOutWithMock(logging, 'FileHandler')
         self.mox.StubOutWithMock(logging, 'Formatter')
+
         logging.getLogger(self.path).AndReturn(logger)
         logging.FileHandler(self.path).AndReturn(logger)
         logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s').AndReturn(logger)
+        
         logger.setLevel(logging.INFO)
         logger.addHandler(logger)
         logger.setFormatter(logger)
