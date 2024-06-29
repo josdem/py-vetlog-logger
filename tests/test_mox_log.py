d66c13f16ce5d8b005d90b809cf8d327394c6bf2
josdem <joseluis.delacruz@gmail.com>
2024-06-29 10:49:39 -0400

    #2 Adding missing mocks

--- tests/test_mox_log.py
+++ tests/test_mox_log.py
@@ -6,18 +6,20 @@ class FixedTest(mox.MoxTestBase):
     path = "vetlog.log"
     def test_create_logger(self):
         logger = self.mox.CreateMockAnything()
+        file_handler = self.mox.CreateMockAnything()
+        formatter = self.mox.CreateMockAnything()
         
         self.mox.StubOutWithMock(logging, 'getLogger')
         self.mox.StubOutWithMock(logging, 'FileHandler')
         self.mox.StubOutWithMock(logging, 'Formatter')
 
         logging.getLogger(self.path).AndReturn(logger)
-        logging.FileHandler(self.path).AndReturn(logger)
-        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s').AndReturn(logger)
-        
+        logging.FileHandler(self.path).AndReturn(file_handler)
+        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s').AndReturn(formatter)
+
         logger.setLevel(logging.INFO)
-        logger.addHandler(logger)
-        logger.setFormatter(logger)
+        logger.addHandler(file_handler)
+        file_handler.setFormatter(formatter)
 
         self.mox.ReplayAll()
         log = Logger(self.path)
